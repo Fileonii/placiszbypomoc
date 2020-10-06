e28fb48f481835259e3ab0821e20c2da309d5ae6
Fileonii <yahhoofj@gmail.com>
2020-10-06 08:50:26 +0200

    Frontend Project started

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <InputTodo />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [kolega_imie, setImie] = useState();
+  const [kolega_zadluzenie, setZadluzenie] = useState();
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { kolega_imie, kolega_zadluzenie };
+      const response = await fetch("http://localhost:5000/friends", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      window.location = "/";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-4">Kalkulator Zadłużenia</h1>
+      <h3 className="text-center text-secondary">placiszbypomoc</h3>
+      <form>
+        <div className="d-flex justify-content-center mt-5">
+          <div class="form-group px-5">
+            <label for="name">Imie Kolegi</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="Imie"
+            />
+          </div>
+          <div class="form-group px-5">
+            <label for="zadluzenie">Zadłużenie</label>
+            <input
+              type="number"
+              class="form-control"
+              id="zadluzenie"
+              placeholder="Kwota"
+            />
+          </div>
+        </div>
+        <div className="form-button mt-3 text-center">
+          <button type="submit" class="btn-lg btn-primary center-block">
+            Submit
+          </button>
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
